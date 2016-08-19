2fef4f536e4f91068dbf6e7c9a813b724c1cfb3d
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2016-08-18 20:58:27 -0500

    tiff.c: Fixed a heap buffer read overrun if buffer not null terminated.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x181500
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 18,15,0
-#define MagickChangeDate   "20160815"
-#define MagickReleaseDate  "snapshot-20160815"
+#define MagickChangeDate   "20160818"
+#define MagickReleaseDate  "snapshot-20160818"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
