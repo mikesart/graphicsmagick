7deb8b352d46488a81cf24ad7f53d8aabea60a2e
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2016-08-22 19:39:13 -0500

    Use MAGICK_CACHE_LINE_SIZE=128 for PowerPC.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x181500
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 18,15,0
-#define MagickChangeDate   "20160821"
-#define MagickReleaseDate  "snapshot-20160821"
+#define MagickChangeDate   "20160822"
+#define MagickReleaseDate  "snapshot-20160822"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
