d5078ea771d86e97cc89777c1aa74a316cece08c
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2016-09-11 18:24:27 -0500

    Fix content of PerlMagick MANIFEST.

--- PerlMagick/build_manifest.sh
+++ PerlMagick/build_manifest.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-find . -type f -print | egrep -v '(.#.*)|(CVS)|(core)|(~$)|(.*\.orig)|(.*\.cvsignore)|(.*\.gdbinit)|(demo/demo.jpg)|(Makefile.in)|(Makefile.old)' | sed -e 's:\./::' | sort > MANIFEST
+# Create a PerlMagick MANIFEST file.  This needs to be run in a clean source tree.
+rm -f MANIFEST
+perl -MExtUtils::Manifest=mkmanifest -e 'mkmanifest()'
+
+#find . -type f -print | egrep -v '(.#.*)|(CVS)|(core)|(.*_tmp\.*)|(perlmain\.*)|(^PerlMagick)|(~$)|(.*\.orig)|(.*\.cvsignore)|(.*\.gdbinit)|(demo/demo.jpg)|(Makefile.in)|(Makefile.old)' | sed -e 's:\./::' | sort > MANIFEST
--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x181501
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 18,15,1
-#define MagickChangeDate   "20160910"
-#define MagickReleaseDate  "snapshot-20160910"
+#define MagickChangeDate   "20160911"
+#define MagickReleaseDate  "snapshot-20160911"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
