e350905ca86fb632a1d93598fbc830fb24aa8cb0
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2017-01-01 13:43:09 -0600

    Bump copyright years and rotate ChangeLog.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x181501
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 18,15,1
-#define MagickChangeDate   ""
-#define MagickReleaseDate  "snapshot-"
+#define MagickChangeDate   "20170101"
+#define MagickReleaseDate  "snapshot-20170101"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
